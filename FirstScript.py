daa290d83cd165ab682532223a7935cd4e065b08
irina <irishka.klimovich.2002@mail.ru>
2021-02-19 20:42:43 +0300

    data validation

--- FirstScript.py
+++ FirstScript.py
@@ -1,19 +1,53 @@
-# 
+# the Gauss method with data from "matrix.txt" and "vector.txt"
+
 import numpy as np
+import os
 
-A = np.loadtxt("matrix.txt", delimiter=' ', dtype=np.float)
-n=np.shape(A)[0]
-m=np.shape(A)[1]
-print("Matrix A:\n", A)
-
-b = np.loadtxt("vector.txt", delimiter=' ', dtype=np.float)
-if np.shape(b)[0]!=n:
+def error():
 	print("Error. Check text files and try again. Bye...")
 	f = open('answer.txt', 'w')
 	f.write("Error. Check matrix.txt and vector.txt and try again.")
-	quit()
-print("Vector b:", b)
+	f.close()
+	pass	
 
+#get matrix from file
+if os.path.isfile("matrix.txt") and os.path.getsize("matrix.txt")>0:
+	try:
+		A = np.loadtxt("matrix.txt", delimiter=' ', dtype=float)
+		n=np.shape(A)[0]
+		m=np.shape(A)[1]
+		print("Matrix A:\n", A)
+	except Exception:
+		error()
+		quit()	
+else:
+	error()
+	quit()
+
+#get vector from file
+if os.path.isfile("vector.txt") and os.path.getsize("vector.txt")>0:
+	try:
+		b = np.loadtxt("vector.txt", delimiter=' ', dtype=float)
+	except Exception:
+		error()
+		quit()	
+	if np.shape(b)[0]!=n:
+		error()
+		quit()
+	print("Vector b:", b)
+else:
+	error()
+	quit()
+
+#checking possibility of solving	
+i=0
+while i<n:
+	if A[i][i]==0.:
+		error()
+		quit()
+	i+=1
+
+# solving system of linear algebraic equations
 t=0
 while t < n:
     i=t+1
@@ -35,8 +69,9 @@ while t > -1:
         b[i]-=x[t]*A[i][t]
         i=i-1
     t=t-1
-print("Solve of the SLAE:", np.round((x), 4))
 
+#print answer
+print("Solve of the SLAE:", np.round((x), 4))
 f = open('answer.txt', 'w')
 for xn in x:
 	a =  str(np.round(xn, 4))
